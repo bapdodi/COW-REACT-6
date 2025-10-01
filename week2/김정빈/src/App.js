71c3fd66ec9b193cf64663fd200ec6d151e10323
bapdodi <arlawjdqls12@gmail.com>
2025-10-01 12:07:04 +0900

    feat: 완료 버튼 생성

--- week2/김정빈/src/App.js
+++ week2/김정빈/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() { // 함수형 컴포넌트 
   const [todos, setTodos] = useState([]); //초기값을 빈 배열로 선언, setTodos는 변경 함수
   const [inputValue, setInputValue] = useState('');
+  const [editingId, setEditingId] = useState(null); // 편집 중인 할 일의 ID
+  const [editingText, setEditingText] = useState(''); // 편집 중인 텍스트
 
   // 할 일 추가 함수
   const addTodo = () => {
@@ -16,6 +18,36 @@ function App() { // 함수형 컴포넌트
   // 할 일 삭제 함수
   const deleteTodo = (id) => {
     setTodos(todos.filter(todo => todo.id !== id)); // 새로운 리스트를 만드는데 키가 일치하는거만 뺌 
+  };
+
+  // 할 일 완료/미완료 토글 함수
+  const toggleTodo = (id) => {
+    setTodos(todos.map(todo => 
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    ));
+  };
+
+  // 편집 시작 함수
+  const startEdit = (id, text) => {
+    setEditingId(id);
+    setEditingText(text);
+  };
+
+  // 편집 저장 함수
+  const saveEdit = (id) => {
+    if (editingText.trim() !== '') {
+      setTodos(todos.map(todo => 
+        todo.id === id ? { ...todo, text: editingText } : todo
+      ));
+    }
+    setEditingId(null);
+    setEditingText('');
+  };
+
+  // 편집 취소 함수
+  const cancelEdit = () => {
+    setEditingId(null);
+    setEditingText('');
   };
 
   return ( // 화면에 표시할 HTML 같은 코드(JSX)를 반환
@@ -38,7 +70,12 @@ function App() { // 함수형 컴포넌트
       <ul>
         {todos.map(todo => ( //각각의 todo
           <li key={todo.id}>
-            {todo.text}
+            <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+              {todo.text}
+            </span>
+            <button onClick={() => toggleTodo(todo.id)}>
+              {todo.completed ? '미완료' : '완료'}
+            </button>
             <button onClick={() => deleteTodo(todo.id)}>삭제</button>
           </li>
         ))}
